62a32f6cf8d99abf7ba42fa11080d0c73fa89e20
sahithmasa <111427134+sahithmasa@users.noreply.github.com>
2022-09-28 11:23:06 +0530

    def np(n):     if n==1:         return 1     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 1     else:         return 0 n=int(input()) c=0 for i in range(1,n+1):     if n%i==0 and np(i):         c+=1 print(c)

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -0,0 +1,15 @@
+def np(n):
+    if n==1:
+        return 1
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 1
+    else:
+        return 0
+n=int(input())
+c=0
+for i in range(1,n+1):
+    if n%i==0 and np(i):
+        c+=1
+print(c)
+        
